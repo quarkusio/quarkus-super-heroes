2c969589a53288999d7f022c7220f8bf22b4cbc9
Eric Deandrea <edeandrea@redhat.com>
2021-11-30 12:47:06 -0500

    Using DevServicesContext for reading kafka properties

--- rest-fights/src/test/java/io/quarkus/sample/superheroes/fight/KafkaBrokerResource.java
+++ rest-fights/src/test/java/io/quarkus/sample/superheroes/fight/KafkaBrokerResource.java
@@ -1,16 +1,17 @@
 package io.quarkus.sample.superheroes.fight;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.utility.DockerImageName;
 
 import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
+import io.quarkus.test.common.DevServicesContext;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager.TestInjector.AnnotatedAndMatchesType;
 
@@ -18,23 +19,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 /**
- * Quarkus {@link QuarkusTestResourceLifecycleManager} wrapping a {@link KafkaContainer}, binding the {@code kafka.bootstrap.servers}, and exposing it to tests that want to inject it via {@link InjectKafkaConsumer}.
+ * Quarkus {@link QuarkusTestResourceLifecycleManager} binding the {@code kafka.bootstrap.servers}, and exposing it to tests that want to inject it via {@link InjectKafkaConsumer}.
  * @see InjectKafkaConsumer
  */
-public class KafkaBrokerResource implements QuarkusTestResourceLifecycleManager {
+public class KafkaBrokerResource implements QuarkusTestResourceLifecycleManager, DevServicesContext.ContextAware {
+	private static final AtomicReference<Map<String, String>> DEV_SERVICES_PROPERTIES = new AtomicReference<>(new HashMap<>());
 	private KafkaConsumer<String, Fight> fightConsumer;
-	private final KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"));
 
 	@Override
 	public Map<String, String> start() {
 		var objectMapper = new ObjectMapper();
 		objectMapper.registerModule(new JavaTimeModule());
 
-		this.kafka.start();
 		this.fightConsumer = new KafkaConsumer<>(consumerConfig(), new StringDeserializer(), new ObjectMapperDeserializer<>(Fight.class, objectMapper));
 		this.fightConsumer.subscribe(List.of("fights"));
 
-		return Map.of("kafka.bootstrap.servers", this.kafka.getBootstrapServers());
+		return Map.of();
 	}
 
 	@Override
@@ -43,15 +43,11 @@ public class KafkaBrokerResource implements QuarkusTestResourceLifecycleManager 
 			this.fightConsumer.unsubscribe();
 			this.fightConsumer.close();
 		}
-
-		if (this.kafka != null) {
-			this.kafka.close();
-		}
 	}
 
 	private Properties consumerConfig() {
 		var properties = new Properties();
-		properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, this.kafka.getBootstrapServers());
+		properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, DEV_SERVICES_PROPERTIES.get().get("kafka.bootstrap.servers"));
 		properties.put(ConsumerConfig.GROUP_ID_CONFIG, "fights");
 		properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
 		properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
@@ -65,5 +61,10 @@ public class KafkaBrokerResource implements QuarkusTestResourceLifecycleManager 
 			this.fightConsumer,
 			new AnnotatedAndMatchesType(InjectKafkaConsumer.class, KafkaConsumer.class)
 		);
+	}
+
+	@Override
+	public void setIntegrationTestContext(DevServicesContext context) {
+		DEV_SERVICES_PROPERTIES.getAndSet(context.devServicesProperties());
 	}
 }
