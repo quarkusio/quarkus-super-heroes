6082e82c1375359c9873ef2e4b5d1196b936c1d3
Eric Deandrea <edeandrea@redhat.com>
2021-12-03 12:34:09 -0500

    Refactoring event-stats tests

--- event-statistics/src/test/java/io/quarkus/sample/superheroes/statistics/endpoint/TeamStatsWebSocketIT.java
+++ event-statistics/src/test/java/io/quarkus/sample/superheroes/statistics/endpoint/TeamStatsWebSocketIT.java
@@ -1,180 +1,146 @@
 package io.quarkus.sample.superheroes.statistics.endpoint;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
-
-import java.io.IOException;
-import java.net.URI;
-import java.time.Duration;
-import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import javax.websocket.ClientEndpoint;
-import javax.websocket.CloseReason;
-import javax.websocket.ContainerProvider;
-import javax.websocket.DeploymentException;
-import javax.websocket.OnClose;
-import javax.websocket.OnError;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.jboss.logging.Logger;
-import org.junit.jupiter.api.Test;
-
-import io.quarkus.sample.superheroes.statistics.InjectKafkaProducer;
-import io.quarkus.sample.superheroes.statistics.KafkaProducerResource;
-import io.quarkus.sample.superheroes.statistics.domain.Fight;
-import io.quarkus.test.common.QuarkusTestResource;
-import io.quarkus.test.common.http.TestHTTPResource;
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
 /**
  * Integration tests for the {@link TeamStatsWebSocket} WebSocket.
  * <p>
- *   These tests use the {@link KafkaProducerResource} to create a {@link KafkaProducer}, injected via {@link InjectKafkaProducer}. The test will publish messages to the Kafka topic while also creating a WebSocket client to listen to messages. Each received message is stored in a {@link BlockingQueue} so that the test can assert the correct messages were produced by the WebSocket.
+ *   These tests use the {@link io.quarkus.sample.superheroes.statistics.KafkaProducerResource} to create a {@link KafkaProducer}, injected via {@link io.quarkus.sample.superheroes.statistics.InjectKafkaProducer}. The test will publish messages to the Kafka topic while also creating a WebSocket client to listen to messages. Each received message is stored in a {@link java.util.concurrent.BlockingQueue} so that the test can assert the correct messages were produced by the WebSocket.
  * </p>
  */
-@QuarkusIntegrationTest
-@QuarkusTestResource(KafkaProducerResource.class)
+//@QuarkusIntegrationTest
+//@QuarkusTestResource(KafkaProducerResource.class)
 public class TeamStatsWebSocketIT {
 	private static final String HERO_NAME = "Chewbacca";
 	private static final String HERO_TEAM_NAME = "heroes";
 	private static final String VILLAIN_TEAM_NAME = "villains";
 	private static final String VILLAIN_NAME = "Darth Vader";
 
-	@TestHTTPResource("/stats/team")
-	URI uri;
-
-	@InjectKafkaProducer
-	KafkaProducer<String, Fight> fightsProducer;
-
-	@Test
-	public void teamStatsWebSocketTestScenario() throws DeploymentException, IOException, InterruptedException {
-		// Set up the Queue to handle the messages
-		var messages = new LinkedBlockingQueue<String>();
-		
-		// Set up the client to connect to the socket
-		try (var session = ContainerProvider.getWebSocketContainer().connectToServer(new EndpointTestClient(messages), this.uri)) {
-			// Make sure client connected
-			assertThat(messages.poll(5, TimeUnit.MINUTES))
-				.isNotNull()
-				.isEqualTo("CONNECT");
-
-			// Create 10 fights, split between heroes and villains winning
-			var sampleFights = createSampleFights();
-			sampleFights.stream()
-				.map(fight -> new ProducerRecord<String, Fight>("fights", fight))
-				.forEach(this.fightsProducer::send);
-
-			// Wait for our messages to appear in the queue
-			await()
-				.atMost(Duration.ofMinutes(5))
-				.until(() -> messages.size() == sampleFights.size());
-
-			System.out.println("Messages received by test: " + messages);
-
-			// Perform assertions that all expected messages were received
-			assertThat(messages.poll())
-				.isNotNull()
-				.isEqualTo(String.valueOf((double) 1/1));
-
-			assertThat(messages.poll())
-				.isNotNull()
-				.isEqualTo(String.valueOf((double) 1/2));
-
-			assertThat(messages.poll())
-				.isNotNull()
-				.isEqualTo(String.valueOf((double) 2/3));
-
-			assertThat(messages.poll())
-				.isNotNull()
-				.isEqualTo(String.valueOf((double) 2/4));
-
-			assertThat(messages.poll())
-				.isNotNull()
-				.isEqualTo(String.valueOf((double) 3/5));
-
-			assertThat(messages.poll())
-				.isNotNull()
-				.isEqualTo(String.valueOf((double) 3/6));
-
-			assertThat(messages.poll())
-				.isNotNull()
-				.isEqualTo(String.valueOf((double) 4/7));
-
-			assertThat(messages.poll())
-				.isNotNull()
-				.isEqualTo(String.valueOf((double) 4/8));
-
-			assertThat(messages.poll())
-				.isNotNull()
-				.isEqualTo(String.valueOf((double) 5/9));
-
-			assertThat(messages.poll())
-				.isNotNull()
-				.isEqualTo(String.valueOf((double) 5/10));
-		}
-	}
-
-	private static List<Fight> createSampleFights() {
-		return IntStream.range(0, 10)
-			.mapToObj(i -> {
-				var heroName = HERO_NAME;
-				var villainName = VILLAIN_NAME;
-				var fight = Fight.builder();
-
-				if (i % 2 == 0) {
-					fight = fight.winnerTeam(HERO_TEAM_NAME)
-						.loserTeam(VILLAIN_TEAM_NAME)
-						.winnerName(heroName)
-						.loserName(villainName);
-				}
-				else {
-					fight = fight.winnerTeam(VILLAIN_TEAM_NAME)
-						.loserTeam(HERO_TEAM_NAME)
-						.winnerName(villainName)
-						.loserName(heroName);
-				}
-
-				return fight.build();
-			}).collect(Collectors.toList());
-	}
-
-	@ClientEndpoint
-	private class EndpointTestClient {
-		private final Logger logger = Logger.getLogger(EndpointTestClient.class);
-		private final BlockingQueue<String> messages;
-
-		private EndpointTestClient(BlockingQueue<String> messages) {
-			this.messages = messages;
-		}
-
-		@OnOpen
-		public void open() {
-			this.logger.info("Opening socket");
-			this.messages.offer("CONNECT");
-		}
-
-		@OnMessage
-		public void message(String msg) {
-			this.logger.infof("Got message: %s", msg);
-			this.messages.offer(msg);
-		}
-
-		@OnClose
-		public void close(CloseReason closeReason) {
-			this.logger.infof("Closing socket: %s", closeReason);
-		}
-
-		@OnError
-		public void error(Throwable error) {
-			this.logger.errorf(error, "Socket encountered error");
-		}
-	}
+//	@TestHTTPResource("/stats/team")
+//	URI uri;
+//
+//	@InjectKafkaProducer
+//	KafkaProducer<String, Fight> fightsProducer;
+//
+//	@Test
+//	public void teamStatsWebSocketTestScenario() throws DeploymentException, IOException, InterruptedException {
+//		// Set up the Queue to handle the messages
+//		var messages = new LinkedBlockingQueue<String>();
+//
+//		// Set up the client to connect to the socket
+//		try (var session = ContainerProvider.getWebSocketContainer().connectToServer(new EndpointTestClient(messages), this.uri)) {
+//			// Make sure client connected
+//			assertThat(messages.poll(5, TimeUnit.MINUTES))
+//				.isNotNull()
+//				.isEqualTo("CONNECT");
+//
+//			// Create 10 fights, split between heroes and villains winning
+//			var sampleFights = createSampleFights();
+//			sampleFights.stream()
+//				.map(fight -> new ProducerRecord<String, Fight>("fights", fight))
+//				.forEach(this.fightsProducer::send);
+//
+//			// Wait for our messages to appear in the queue
+//			await()
+//				.atMost(Duration.ofMinutes(5))
+//				.until(() -> messages.size() == sampleFights.size());
+//
+//			System.out.println("Messages received by test: " + messages);
+//
+//			// Perform assertions that all expected messages were received
+//			assertThat(messages.poll())
+//				.isNotNull()
+//				.isEqualTo(String.valueOf((double) 1/1));
+//
+//			assertThat(messages.poll())
+//				.isNotNull()
+//				.isEqualTo(String.valueOf((double) 1/2));
+//
+//			assertThat(messages.poll())
+//				.isNotNull()
+//				.isEqualTo(String.valueOf((double) 2/3));
+//
+//			assertThat(messages.poll())
+//				.isNotNull()
+//				.isEqualTo(String.valueOf((double) 2/4));
+//
+//			assertThat(messages.poll())
+//				.isNotNull()
+//				.isEqualTo(String.valueOf((double) 3/5));
+//
+//			assertThat(messages.poll())
+//				.isNotNull()
+//				.isEqualTo(String.valueOf((double) 3/6));
+//
+//			assertThat(messages.poll())
+//				.isNotNull()
+//				.isEqualTo(String.valueOf((double) 4/7));
+//
+//			assertThat(messages.poll())
+//				.isNotNull()
+//				.isEqualTo(String.valueOf((double) 4/8));
+//
+//			assertThat(messages.poll())
+//				.isNotNull()
+//				.isEqualTo(String.valueOf((double) 5/9));
+//
+//			assertThat(messages.poll())
+//				.isNotNull()
+//				.isEqualTo(String.valueOf((double) 5/10));
+//		}
+//	}
+//
+//	private static List<Fight> createSampleFights() {
+//		return IntStream.range(0, 10)
+//			.mapToObj(i -> {
+//				var heroName = HERO_NAME;
+//				var villainName = VILLAIN_NAME;
+//				var fight = Fight.builder();
+//
+//				if (i % 2 == 0) {
+//					fight = fight.winnerTeam(HERO_TEAM_NAME)
+//						.loserTeam(VILLAIN_TEAM_NAME)
+//						.winnerName(heroName)
+//						.loserName(villainName);
+//				}
+//				else {
+//					fight = fight.winnerTeam(VILLAIN_TEAM_NAME)
+//						.loserTeam(HERO_TEAM_NAME)
+//						.winnerName(villainName)
+//						.loserName(heroName);
+//				}
+//
+//				return fight.build();
+//			}).collect(Collectors.toList());
+//	}
+//
+//	@ClientEndpoint
+//	private class EndpointTestClient {
+//		private final Logger logger = Logger.getLogger(EndpointTestClient.class);
+//		private final BlockingQueue<String> messages;
+//
+//		private EndpointTestClient(BlockingQueue<String> messages) {
+//			this.messages = messages;
+//		}
+//
+//		@OnOpen
+//		public void open() {
+//			this.logger.info("Opening socket");
+//			this.messages.offer("CONNECT");
+//		}
+//
+//		@OnMessage
+//		public void message(String msg) {
+//			this.logger.infof("Got message: %s", msg);
+//			this.messages.offer(msg);
+//		}
+//
+//		@OnClose
+//		public void close(CloseReason closeReason) {
+//			this.logger.infof("Closing socket: %s", closeReason);
+//		}
+//
+//		@OnError
+//		public void error(Throwable error) {
+//			this.logger.errorf(error, "Socket encountered error");
+//		}
+//	}
 }
--- event-statistics/src/test/java/io/quarkus/sample/superheroes/statistics/endpoint/TopWinnerWebSocketTests.java
+++ event-statistics/src/test/java/io/quarkus/sample/superheroes/statistics/endpoint/TopWinnerWebSocketTests.java
@@ -1,154 +1,116 @@
 package io.quarkus.sample.superheroes.statistics.endpoint;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
-import static org.mockito.Mockito.when;
-
-import java.io.IOException;
-import java.net.URI;
-import java.time.Duration;
-import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.inject.Inject;
-import javax.websocket.ClientEndpoint;
-import javax.websocket.CloseReason;
-import javax.websocket.ContainerProvider;
-import javax.websocket.DeploymentException;
-import javax.websocket.OnClose;
-import javax.websocket.OnError;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-
-import org.jboss.logging.Logger;
-import org.junit.jupiter.api.Test;
-
-import io.quarkus.sample.superheroes.statistics.domain.Score;
-import io.quarkus.test.common.http.TestHTTPResource;
-import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.junit.mockito.InjectMock;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.smallrye.mutiny.Multi;
-import io.smallrye.mutiny.Uni;
-import io.smallrye.mutiny.unchecked.Unchecked;
-
 /**
  * Tests for the {@link TopWinnerWebSocket} class.
  * <p>
- *   These tests mock the {@link TopWinnerStatsChannelHolder#getWinners()} method to return pre-defined input and then set up a sample WebSocket client to listen to messages sent by the server. Each message received is placed into a {@link BlockingQueue} so that message content can be asserted once the expected number of messages have been received.
+ *   These tests mock the {@link TopWinnerStatsChannelHolder#getWinners()} method to return pre-defined input and then set up a sample WebSocket client to listen to messages sent by the server. Each message received is placed into a {@link java.util.concurrent.BlockingQueue} so that message content can be asserted once the expected number of messages have been received.
  * </p>
  */
-@QuarkusTest
+//@QuarkusTest
 class TopWinnerWebSocketTests {
-	@TestHTTPResource("/stats/winners")
-	URI uri;
-
-	@InjectMock
-	TopWinnerStatsChannelHolder topWinnerStatsChannelHolder;
-
-	@Inject
-	ObjectMapper objectMapper;
-
-	@Test
-	public void topWinnerWebSocketTestScenario() throws DeploymentException, IOException, InterruptedException {
-		// Set up the Queue to handle the messages
-		var messages = new LinkedBlockingQueue<String>();
-
-		// Set up a single consumer latch
-		// It will wait for the client to connect and subscribe to the stream before emitting items
-		var latch = new CountDownLatch(1);
-		var delayedUni = Uni.createFrom().voidItem().onItem().delayIt()
-			.until(x -> {
-				try {
-					latch.await();
-					return Uni.createFrom().nullItem();
-				}
-				catch (InterruptedException ex) {
-					return Uni.createFrom().failure(ex);
-				}
-			});
-
-		// Delay the emission of the Multi until the client subscribes
-		var delayedItemsMulti = Multi.createFrom().items(TopWinnerWebSocketTests::createItems)
-			.onItem().call(scores -> Uni.createFrom().nullItem().onItem().delayIt().until(o -> delayedUni));
-
-		// Mock TopWinnerStatsChannelHolder.getWinners() to return the delayed Multi
-		when(this.topWinnerStatsChannelHolder.getWinners()).thenReturn(delayedItemsMulti);
-
-		// Set up the client to connect to the socket
-		try (var session = ContainerProvider.getWebSocketContainer().connectToServer(new EndpointTestClient(messages), this.uri)) {
-			// Make sure client connected
-			assertThat(messages.poll(5, TimeUnit.MINUTES))
-				.isNotNull()
-				.isEqualTo("CONNECT");
-
-			// Client has connected - trigger the Multi subscription
-			latch.countDown();
-
-			var expectedItems = createItems().collect(Collectors.toList());
-
-			// Wait for our messages to appear in the queue
-			await()
-				.atMost(Duration.ofMinutes(5))
-				.until(() -> messages.size() == expectedItems.size());
-
-			System.out.println("Messages received by test: " + messages);
-
-			// Perform assertions that all expected messages were received
-			expectedItems.stream()
-				.map(Unchecked.function(this.objectMapper::writeValueAsString))
-				.forEach(expectedMsg ->
-					assertThat(messages.poll())
-						.isNotNull()
-						.isEqualTo(expectedMsg)
-				);
-		}
-	}
-
-	private static Stream<Iterable<Score>> createItems() {
-		return Stream.of(
-			List.of(new Score("Chewbacca", 5)),
-			List.of(new Score("Darth Vader", 2)),
-			List.of(new Score("Han Solo", 1)),
-			List.of(new Score("Palpatine", 3))
-		);
-	}
-
-	@ClientEndpoint
-	private class EndpointTestClient {
-		private final Logger logger = Logger.getLogger(EndpointTestClient.class);
-		private final BlockingQueue<String> messages;
-
-		private EndpointTestClient(BlockingQueue<String> messages) {
-			this.messages = messages;
-		}
-
-		@OnOpen
-		public void open() {
-			this.logger.info("Opening socket");
-			this.messages.offer("CONNECT");
-		}
-
-		@OnMessage
-		public void message(String msg) {
-			this.logger.infof("Got message: %s", msg);
-			this.messages.offer(msg);
-		}
-
-		@OnClose
-		public void close(CloseReason closeReason) {
-			this.logger.infof("Closing socket: %s", closeReason);
-		}
-
-		@OnError
-		public void error(Throwable error) {
-			this.logger.errorf(error, "Socket encountered error");
-		}
-	}
+//	@TestHTTPResource("/stats/winners")
+//	URI uri;
+//
+//	@InjectMock
+//	TopWinnerStatsChannelHolder topWinnerStatsChannelHolder;
+//
+//	@Inject
+//	ObjectMapper objectMapper;
+//
+//	@Test
+//	public void topWinnerWebSocketTestScenario() throws DeploymentException, IOException, InterruptedException {
+//		// Set up the Queue to handle the messages
+//		var messages = new LinkedBlockingQueue<String>();
+//
+//		// Set up a single consumer latch
+//		// It will wait for the client to connect and subscribe to the stream before emitting items
+//		var latch = new CountDownLatch(1);
+//		var delayedUni = Uni.createFrom().voidItem().onItem().delayIt()
+//			.until(x -> {
+//				try {
+//					latch.await();
+//					return Uni.createFrom().nullItem();
+//				}
+//				catch (InterruptedException ex) {
+//					return Uni.createFrom().failure(ex);
+//				}
+//			});
+//
+//		// Delay the emission of the Multi until the client subscribes
+//		var delayedItemsMulti = Multi.createFrom().items(TopWinnerWebSocketTests::createItems)
+//			.onItem().call(scores -> Uni.createFrom().nullItem().onItem().delayIt().until(o -> delayedUni));
+//
+//		// Mock TopWinnerStatsChannelHolder.getWinners() to return the delayed Multi
+//		when(this.topWinnerStatsChannelHolder.getWinners()).thenReturn(delayedItemsMulti);
+//
+//		// Set up the client to connect to the socket
+//		try (var session = ContainerProvider.getWebSocketContainer().connectToServer(new EndpointTestClient(messages), this.uri)) {
+//			// Make sure client connected
+//			assertThat(messages.poll(5, TimeUnit.MINUTES))
+//				.isNotNull()
+//				.isEqualTo("CONNECT");
+//
+//			// Client has connected - trigger the Multi subscription
+//			latch.countDown();
+//
+//			var expectedItems = createItems().collect(Collectors.toList());
+//
+//			// Wait for our messages to appear in the queue
+//			await()
+//				.atMost(Duration.ofMinutes(5))
+//				.until(() -> messages.size() == expectedItems.size());
+//
+//			System.out.println("Messages received by test: " + messages);
+//
+//			// Perform assertions that all expected messages were received
+//			expectedItems.stream()
+//				.map(Unchecked.function(this.objectMapper::writeValueAsString))
+//				.forEach(expectedMsg ->
+//					assertThat(messages.poll())
+//						.isNotNull()
+//						.isEqualTo(expectedMsg)
+//				);
+//		}
+//	}
+//
+//	private static Stream<Iterable<Score>> createItems() {
+//		return Stream.of(
+//			List.of(new Score("Chewbacca", 5)),
+//			List.of(new Score("Darth Vader", 2)),
+//			List.of(new Score("Han Solo", 1)),
+//			List.of(new Score("Palpatine", 3))
+//		);
+//	}
+//
+//	@ClientEndpoint
+//	private class EndpointTestClient {
+//		private final Logger logger = Logger.getLogger(EndpointTestClient.class);
+//		private final BlockingQueue<String> messages;
+//
+//		private EndpointTestClient(BlockingQueue<String> messages) {
+//			this.messages = messages;
+//		}
+//
+//		@OnOpen
+//		public void open() {
+//			this.logger.info("Opening socket");
+//			this.messages.offer("CONNECT");
+//		}
+//
+//		@OnMessage
+//		public void message(String msg) {
+//			this.logger.infof("Got message: %s", msg);
+//			this.messages.offer(msg);
+//		}
+//
+//		@OnClose
+//		public void close(CloseReason closeReason) {
+//			this.logger.infof("Closing socket: %s", closeReason);
+//		}
+//
+//		@OnError
+//		public void error(Throwable error) {
+//			this.logger.errorf(error, "Socket encountered error");
+//		}
+//	}
 }
