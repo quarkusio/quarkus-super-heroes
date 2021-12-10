da5a472556c62f09fe0a4c3f26cbead44dd2a433
Eric Deandrea <edeandrea@redhat.com>
2021-12-10 13:08:16 -0500

    Fix ui Dockerfile

--- ui-super-heroes/serving/server.js
+++ ui-super-heroes/serving/server.js
@@ -3,13 +3,17 @@
 const express = require('express')
 const log = require('barelog');
 const { join } = require('path');
-const { writeFileSync } = require('fs');
+// const { writeFileSync } = require('fs');
 const { HTTP_PORT, STATIC_DIR, API_BASE_URL } = require('./config')
 
 const app = express();
 
 // Generate the env.js file inside the dist folder
-writeFileSync(join(STATIC_DIR, 'env.js'), `window.NG_CONFIG={ API_BASE_URL: "${API_BASE_URL}" }`)
+// writeFileSync(join(STATIC_DIR, 'env.js'), `window.NG_CONFIG={ API_BASE_URL: "${API_BASE_URL}" }`)
+app.get('/env.js', (req, res) => {
+    res.setHeader('Content-Type', 'application/javascript');
+    res.send(`window.NG_CONFIG={ API_BASE_URL: "${API_BASE_URL}" }`);
+})
 
 // Serve files from the configured static directory
 app.use(express.static(STATIC_DIR))
