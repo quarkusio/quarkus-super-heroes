7918d3ae2a677761aa930918e4c24a06c34ffbea
Eric Deandrea <edeandrea@redhat.com>
2023-10-11 11:19:40 -0400

    Update ConfigChecker.java
    Adding some documentation to ConfigChecker

--- rest-narration/src/main/java/io/quarkus/sample/superheroes/narration/config/ConfigChecker.java
+++ rest-narration/src/main/java/io/quarkus/sample/superheroes/narration/config/ConfigChecker.java
@@ -4,6 +4,10 @@ import jakarta.enterprise.context.ApplicationScoped;
 
 import io.quarkus.runtime.Startup;
 
+/**
+ * This class is needed to ensure that the {@link NarrationConfig} is valid upon application startup.
+ * <p>If any of the validations fail then the application will fail to start up, which is what we want.</p>
+**/
 @Startup
 @ApplicationScoped
 public class ConfigChecker {
