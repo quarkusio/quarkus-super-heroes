e6ab96344f58e348ad0011c45dc77192e574ca62
Eric Deandrea <edeandrea@redhat.com>
2023-10-23 08:43:35 -0400

    Update all-locations.sql

--- all-locations.sql
+++ all-locations.sql
@@ -1,7 +1,7 @@
-INSERT INTO locations(id, name, description, picture, type) 
+INSERT INTO locations(id, name, description, picture, type)
 VALUES (next value for locations_SEQ, 'Gotham City', 'An American city rife with corruption and crime, the home of its iconic protector Batman.', 'https://raw.githubusercontent.com/quarkusio/quarkus-super-heroes/characterdata/images/locations/gotham_city.jpg', 'CITY');
 INSERT INTO locations(id, name, description, picture, type)
-VALUES (next value for locations_SEQ, 'Earth', 'Earth—our home planet—is the only place we know of so far that’s inhabited by living things.', 'https://raw.githubusercontent.com/quarkusio/quarkus-super-heroes/characterdata/images/locations/earth.jpg', 'PLANET');
+VALUES (next value for locations_SEQ, 'Earth', 'Earth, our home planet, is the only place we know of so far that is inhabited by living things.', 'https://raw.githubusercontent.com/quarkusio/quarkus-super-heroes/characterdata/images/locations/earth.jpg', 'PLANET');
 INSERT INTO locations(id, name, description, picture, type)
 VALUES (next value for locations_SEQ, 'Krypton', 'An ancient world, Krypton was home to advanced civilization known as Kryptonians. In one reality they possessed superhuman strength among other abilities far beyond those of Earth humans. In the majority of realities, these powers would not appear unless the Kryptonian was offworld or under the light of an alien sun.', 'https://raw.githubusercontent.com/quarkusio/quarkus-super-heroes/characterdata/images/locations/krypton.jpg', 'PLANET');
 INSERT INTO locations(id, name, description, picture, type)
