1665a81f6b60c1bc8b5bec322c70f8b09f118ae4
Eric Deandrea <edeandrea@redhat.com>
2023-11-13 10:08:18 -0500

    Adding Wakanda location

--- grpc-locations/src/test/kotlin/io/quarkus/sample/superheroes/location/grpc/LocationGrpcServiceIT.kt
+++ grpc-locations/src/test/kotlin/io/quarkus/sample/superheroes/location/grpc/LocationGrpcServiceIT.kt
@@ -28,7 +28,7 @@ class LocationGrpcServiceIT {
 		private const val DEFAULT_DESCRIPTION = "An American city rife with corruption and crime, the home of its iconic protector Batman."
 		private const val DEFAULT_PICTURE = "https://raw.githubusercontent.com/quarkusio/quarkus-super-heroes/characterdata/images/locations/gotham_city.jpg"
 		private val DEFAULT_TYPE = LocationType.CITY
-		private var NB_LOCATIONS = 30
+		private var NB_LOCATIONS = 31
 		private const val DEFAULT_ORDER = 0
 
 		private lateinit var channel: ManagedChannel
