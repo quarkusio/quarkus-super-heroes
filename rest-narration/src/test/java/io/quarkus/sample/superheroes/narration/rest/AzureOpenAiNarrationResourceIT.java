03a3dcc77138dc29be1b63da5ca13ebbb5f51135
Eric Deandrea <edeandrea@redhat.com>
2026-06-04 13:43:22 -0400

    Remove unused DALLE-3 configurations and simplify test overrides
    Signed-off-by: Eric Deandrea <edeandrea@redhat.com>

--- rest-narration/src/test/java/io/quarkus/sample/superheroes/narration/rest/AzureOpenAiNarrationResourceIT.java
+++ rest-narration/src/test/java/io/quarkus/sample/superheroes/narration/rest/AzureOpenAiNarrationResourceIT.java
@@ -2,7 +2,6 @@ package io.quarkus.sample.superheroes.narration.rest;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import jakarta.ws.rs.core.HttpHeaders;
@@ -128,23 +127,14 @@ class AzureOpenAiNarrationResourceIT extends NarrationResourceIT {
     public Map<String, String> getConfigOverrides() {
 	    var hostname = Boolean.getBoolean("quarkus.container-image.build") ? "host.docker.internal" : "localhost";
 
-      var vals = new HashMap<>(Map.of(
+      return Map.of(
 				"quarkus.langchain4j.azure-openai.enable-integration", "true",
         "quarkus.langchain4j.azure-openai.log-requests", "true",
         "quarkus.langchain4j.azure-openai.log-responses", "true",
         "quarkus.langchain4j.azure-openai.endpoint", "http://%s:${%s}/v1/".formatted(hostname, WireMockConfigKey.PORT),
         "quarkus.langchain4j.azure-openai.max-retries", "2",
         "quarkus.langchain4j.azure-openai.timeout", "3s"
-      ));
-
-      vals.put("quarkus.langchain4j.azure-openai.dalle3.enable-integration", vals.get("quarkus.langchain4j.azure-openai.enable-integration"));
-      vals.put("quarkus.langchain4j.azure-openai.dalle3.log-requests", vals.get("quarkus.langchain4j.azure-openai.log-requests"));
-      vals.put("quarkus.langchain4j.azure-openai.dalle3.log-responses", vals.get("quarkus.langchain4j.azure-openai.log-responses"));
-      vals.put("quarkus.langchain4j.azure-openai.dalle3.endpoint", vals.get("quarkus.langchain4j.azure-openai.endpoint"));
-      vals.put("quarkus.langchain4j.azure-openai.dalle3.max-retries", vals.get("quarkus.langchain4j.azure-openai.max-retries"));
-      vals.put("quarkus.langchain4j.azure-openai.dalle3.timeout", vals.get("quarkus.langchain4j.azure-openai.timeout"));
-
-      return vals;
+      );
     }
   }
 }
--- rest-narration/src/test/java/io/quarkus/sample/superheroes/narration/rest/OpenAiNarrationResourceIT.java
+++ rest-narration/src/test/java/io/quarkus/sample/superheroes/narration/rest/OpenAiNarrationResourceIT.java
@@ -2,7 +2,6 @@ package io.quarkus.sample.superheroes.narration.rest;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import jakarta.ws.rs.core.HttpHeaders;
@@ -127,23 +126,14 @@ class OpenAiNarrationResourceIT extends NarrationResourceIT {
     public Map<String, String> getConfigOverrides() {
 	    var hostname = Boolean.getBoolean("quarkus.container-image.build") ? "host.docker.internal" : "localhost";
 
-      var vals =  new HashMap<>(Map.of(
+      return Map.of(
 				"quarkus.langchain4j.openai.enable-integration", "true",
         "quarkus.langchain4j.openai.log-requests", "true",
         "quarkus.langchain4j.openai.log-responses", "true",
         "quarkus.langchain4j.openai.base-url", "http://%s:${%s}/v1/".formatted(hostname, WireMockConfigKey.PORT),
         "quarkus.langchain4j.openai.max-retries", "2",
         "quarkus.langchain4j.openai.timeout", "3s"
-      ));
-
-//      vals.put("quarkus.langchain4j.openai.dalle3.enable-integration", vals.get("quarkus.langchain4j.openai.enable-integration"));
-//      vals.put("quarkus.langchain4j.openai.dalle3.log-requests", vals.get("quarkus.langchain4j.openai.log-requests"));
-//      vals.put("quarkus.langchain4j.openai.dalle3.log-responses", vals.get("quarkus.langchain4j.openai.log-responses"));
-//      vals.put("quarkus.langchain4j.openai.dalle3.base-url", vals.get("quarkus.langchain4j.openai.base-url"));
-//      vals.put("quarkus.langchain4j.openai.dalle3.max-retries", vals.get("quarkus.langchain4j.openai.max-retries"));
-//      vals.put("quarkus.langchain4j.openai.dalle3.timeout", vals.get("quarkus.langchain4j.openai.timeout"));
-
-      return vals;
+      );
     }
   }
 }
