85853be7a8f55ff450da3ee7135222d899f02c78
Eric Deandrea <edeandrea@redhat.com>
2026-06-04 11:06:20 -0400

    Update OpenAI image model quality to "low" and add moderation support
    Signed-off-by: Eric Deandrea <edeandrea@redhat.com>

--- rest-narration/deploy-openai-key.sh
+++ rest-narration/deploy-openai-key.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+oc delete secret rest-narration-config-creds --ignore-not-found=true
 oc create secret generic rest-narration-config-creds --from-literal=quarkus.langchain4j.openai.api-key="${OPENAI_API_KEY}"
 oc set env deployment/rest-narration --from=secret/rest-narration-config-creds
 oc rollout restart deployment/rest-narration
--- rest-narration/src/main/java/io/quarkus/sample/superheroes/narration/jackson/GenerateImagesRequestMixin.java
+++ rest-narration/src/main/java/io/quarkus/sample/superheroes/narration/jackson/GenerateImagesRequestMixin.java
@@ -2,10 +2,40 @@ package io.quarkus.sample.superheroes.narration.jackson;
 
 import io.quarkus.jackson.JacksonMixin;
 
+import io.quarkus.sample.superheroes.narration.jackson.GenerateImagesRequestMixin.ModerationPropertyWriter;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import com.fasterxml.jackson.databind.cfg.MapperConfig;
+import com.fasterxml.jackson.databind.introspect.AnnotatedClass;
+import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
+import com.fasterxml.jackson.databind.ser.VirtualBeanPropertyWriter;
+import com.fasterxml.jackson.databind.util.Annotations;
 import dev.langchain4j.model.openai.internal.image.GenerateImagesRequest;
 
 @JacksonMixin(GenerateImagesRequest.class)
-@JsonIgnoreProperties("style")
+@JsonAppend(props = @JsonAppend.Prop(value = ModerationPropertyWriter.class, name = "moderation", type = String.class))
+@JsonIgnoreProperties({ "style", "response_format" })
 public interface GenerateImagesRequestMixin {
+  class ModerationPropertyWriter extends VirtualBeanPropertyWriter {
+    public ModerationPropertyWriter() {
+    }
+
+    public ModerationPropertyWriter(BeanPropertyDefinition propDef, Annotations contextAnnotations, JavaType declaredType) {
+      super(propDef, contextAnnotations, declaredType);
+    }
+
+    @Override
+    protected Object value(Object bean, JsonGenerator gen, SerializerProvider prov) {
+      return "low";
+    }
+
+    @Override
+    public VirtualBeanPropertyWriter withConfig(MapperConfig<?> config, AnnotatedClass declaringClass, BeanPropertyDefinition propDef, JavaType type) {
+      return new ModerationPropertyWriter(propDef, declaringClass.getAnnotations(), type);
+    }
+  }
 }
