fa0cb185c2204e7067ebf5d54658fb1a8014f6fb
Eric Deandrea <edeandrea@redhat.com>
2026-06-04 11:43:58 -0400

    Enhance image generation with moderation, safety checks, and improved URL handling. Adjust chat model parameters.
    Signed-off-by: Eric Deandrea <edeandrea@redhat.com>

--- rest-narration/src/main/java/io/quarkus/sample/superheroes/narration/service/ImageGenerationService.java
+++ rest-narration/src/main/java/io/quarkus/sample/superheroes/narration/service/ImageGenerationService.java
@@ -1,5 +1,7 @@
 package io.quarkus.sample.superheroes.narration.service;
 
+import java.util.Optional;
+
 import jakarta.enterprise.context.ApplicationScoped;
 
 import io.quarkus.logging.Log;
@@ -7,6 +9,7 @@ import io.quarkus.logging.Log;
 import io.quarkus.sample.superheroes.narration.FightImage;
 
 import dev.langchain4j.data.image.Image;
+import dev.langchain4j.service.SystemMessage;
 import io.opentelemetry.instrumentation.annotations.SpanAttribute;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 import io.quarkiverse.langchain4j.RegisterAiService;
@@ -14,13 +17,24 @@ import io.quarkiverse.langchain4j.RegisterAiService;
 @RegisterAiService
 @ApplicationScoped
 public interface ImageGenerationService {
+  @SystemMessage("""
+    You are tasked with generating an image from a narration. The narration is intended to be funny.
+    
+    If it does not pass your moderation filter, please feel free to rewrite it and generate an image.
+    """)
   Image generateImage(String narration);
 
   @WithSpan("ImageGenerationService.generateImageForNarration")
   default FightImage generateImageForNarration(@SpanAttribute("arg.narration") String narration) {
     Log.debugf("Generating image for narration: %s", narration);
     var image = generateImage(narration);
+    var imageUrl = Optional.ofNullable(image.url())
+      .map(url -> url.toString())
+      .orElseGet(() -> "data:%s;base64,%s".formatted(Optional.ofNullable(image.mimeType()).orElse("image/png"), image.base64Data()));
 
-    return new FightImage(image.url().toString(), image.revisedPrompt());
+    var imageNarration = Optional.ofNullable(image.revisedPrompt())
+      .orElse(narration);
+
+    return new FightImage(imageUrl, imageNarration);
   }
 }
