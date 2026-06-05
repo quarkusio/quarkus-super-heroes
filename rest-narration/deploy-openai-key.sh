d6938a4f4d908d7c47c7145986d6962458e9a28f
Eric Deandrea <eric.deandrea@ibm.com>
2026-06-05 11:48:10 -0400

    Set default Kafka replication factors and streamline init task flags (#2132)
    Signed-off-by: Eric Deandrea <edeandrea@redhat.com>

--- rest-narration/deploy-openai-key.sh
+++ rest-narration/deploy-openai-key.sh
@@ -5,4 +5,6 @@ set -e
 oc delete secret rest-narration-config-creds --ignore-not-found=true
 oc create secret generic rest-narration-config-creds --from-literal=quarkus.langchain4j.openai.api-key="${OPENAI_API_KEY}"
 oc set env deployment/rest-narration --from=secret/rest-narration-config-creds
+oc set env deployment/rest-narration QUARKUS_PROFILE=openai
+oc set env deployment/rest-narration QUARKUS_LANGCHAIN4J_OPENAI_TIMEOUT=2m
 oc rollout restart deployment/rest-narration
